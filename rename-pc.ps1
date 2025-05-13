fb859aefc9ff7263147ccf48bf929b31e5cdfc1c
nc-lm-pr <erdni.b@netcostmarket.com>
2025-05-13 11:24:33 -0400

    Update rename-pc.ps1

--- rename-pc.ps1
+++ rename-pc.ps1
@@ -1,2 +1,8 @@
-# get serial number
-(Get-WmiObject Win32_BIOS).SerialNumber.Trim()
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
+$newName = (Get-WmiObject Win32_BIOS).SerialNumber.Trim()
+$currentName = $env:COMPUTERNAME
+
+if ($currentName -ne $newName) {
+    Rename-Computer -NewName $newName -DomainCredential -Force -Restart 
+}
