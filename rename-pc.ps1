314fde776e30149dbfbd21d421bfd4cb528b824d
nc-lm-pr <erdni.b@netcostmarket.com>
2025-05-13 11:35:16 -0400

    Update rename-pc.ps1

--- rename-pc.ps1
+++ rename-pc.ps1
@@ -1,8 +1,12 @@
+# Open powershell as Admin
+# Enable script execution for current session
 Set-ExecutionPolicy Bypass -Scope Process -Force
 
+# get domain creds. e.g (domain\username) and password
+$creds = Get-Credential
 $newName = (Get-WmiObject Win32_BIOS).SerialNumber.Trim()
 $currentName = $env:COMPUTERNAME
 
 if ($currentName -ne $newName) {
-    Rename-Computer -NewName $newName -DomainCredential -Force -Restart 
+    Rename-Computer -NewName $newName -DomainCredential $creds -Force -Restart 
 }
